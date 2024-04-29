8751b7414ace13c434b8d0c0fd407c292f136953
TristanL06 <tristan.larguier@gmail.com>
2024-04-29 15:53:10 +0200

    sugar

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 import os
 import subprocess
 import time
 
-input = "valve.png"
+input = "oazo.png"
 
 def transfer_to_boole(source="", destination=""):
     os.system("scp ./" + source + " boole:projet/" + destination)
@@ -28,22 +28,32 @@ transfer_to_boole("project-gpu.py")
 transfer_to_boole(input, "input.jpg")
 #print(execute(["python3 projet.py"]))
 print(execute([
-    "python3 project-gpu.py input.jpg output_test1.jpg --bw",
-    "python3 project-gpu.py input.jpg output_test2.jpg --gauss",
-    "python3 project-gpu.py input.jpg output_test3.jpg --sobel",
-    "python3 project-gpu.py input.jpg output_test4.jpg --threshold",
-    "python3 project-gpu.py input.jpg output_test5.jpg"]))
-transfer_from_boole("out*")
+    "python3 project-gpu.py input.jpg output1_bw.jpg --bw",
+    "python3 project-gpu.py input.jpg output2_gauss.jpg --gauss",
+    "python3 project-gpu.py input.jpg output3_sobel.jpg --sobel",
+    "python3 project-gpu.py input.jpg output4_threshold.jpg --threshold",
+    "python3 project-gpu.py input.jpg output5_final.jpg"]))
+transfer_from_boole("output*")
+print(execute(["rm output*"]))
 
-images = [f for f in os.listdir() if f.startswith("output_test")]
+images = [f for f in os.listdir() if f.startswith("output")]
 
 input_image = Image.open(input)
 x, y = input_image.size
-mixed = Image.new("RGB", ((5 + x) * (len(images)+1) - 5, y) , (255, 255, 255))
+mixed = Image.new("RGB", ((5 + x) * (len(images)+1) - 5, y +x//12) , (255, 255, 255))
 mixed.paste(input_image, (0, 0))
+draw = ImageDraw.Draw(mixed)
+font = ImageFont.load_default(size=x/20)
+text_size = draw.textlength("Image originale", font=font)
+draw.text(((x-text_size)/2, y), "Image originale", (0, 0, 0), font=font)
 # compile all images in one
 for i in range(len(images)):
+    text = images[i].split("_")[1].split(".")[0]
     mixed.paste(Image.open(images[i]), ((x + 5)*(i+1), 0))
+    text_size = draw.textlength(text, font=font)
+    draw.text((x + 5 + (x + 5)*i + (x-text_size)/2, y), text, (0, 0, 0), font=font)
+    
+
 mixed.save("mixed.jpg")
 
 mixed = Image.open("mixed.jpg")
